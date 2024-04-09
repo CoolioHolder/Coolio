fc4eee00bc7a98b74e5c88b0c81d4401d7a10bbd
Coolio <156616687+cooliowebsite@users.noreply.github.com>
2024-04-08 22:31:51 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function enableCloaking() {
     const pageTitle = document.title;
     const icon = document.querySelector("link[rel~='icon']");
-    icon.href = "textures/classroom.webp";
+    icon.href = "https://raw.githubusercontent.com/cooliowebsite/cooliowebsite.github.io/main/textures/icons/classroom.webp";
     localStorage.setItem('cloakingEnabled', true);
     
     // Set the global title
