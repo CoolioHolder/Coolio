05b15dfe6cf1a935d322eacf8c3d459f48d9fe8a
Coolio <156616687+w58fj3ngj@users.noreply.github.com>
2025-01-06 23:14:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,84 +1,47 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-  
-  {
+particlesJS('particles-js', 
+{
   "particles": {
     "number": {
-      "value": 12,
+      "value": 150, // Increase the number of particles for a denser snowfall
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#ffffff" // Snowflakes are white
     },
     "shape": {
-      "type": "circle",
+      "type": "circle", // Snowflakes are circular
       "stroke": {
         "width": 0,
         "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
       }
     },
     "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0,
-        "sync": false
-      }
+      "value": 0.8, // Slightly transparent snowflakes
+      "random": true // Random opacity for a more natural look
     },
     "size": {
-      "value": 4,
-      "random": true,
+      "value": 3, // Snowflake size
+      "random": true, // Random sizes for variety
       "anim": {
-        "enable": false,
-        "speed": 10,
-        "size_min": 0.1,
-        "sync": false
+        "enable": false
       }
     },
     "line_linked": {
-      "enable": true,
-      "distance": 80,
-      "color": "#ffffff",
-      "opacity": 0.3,
-      "width": 1
+      "enable": false // Disable connecting lines for a cleaner look
     },
     "move": {
       "enable": true,
-      "speed": 3,
-      "direction": "none",
+      "speed": 1, // Slow speed for a gentle snowfall
+      "direction": "bottom", // Snow falls downward
       "random": true,
       "straight": false,
-      "out_mode": "out",
-      "bounce": true,
+      "out_mode": "out", // Particles leave the canvas
+      "bounce": false,
       "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
+        "enable": false
       }
     }
   },
@@ -86,42 +49,14 @@ particlesJS('particles-js',
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false,
-        "mode": "repulse"
+        "enable": false // No interaction on hover
       },
       "onclick": {
-        "enable": true,
-        "mode": "push"
+        "enable": false // No interaction on click
       },
       "resize": true
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 50,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
+    "modes": {}
   },
   "retina_detect": true
-}
-
-);
+});
